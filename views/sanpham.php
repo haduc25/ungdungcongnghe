019a24592bc7ad738fa2953a96ff874107ddc0ff
Ha Duc <haducvcvb@gmail.com>
2023-08-21 09:28:11 +0700

    handle sanpham_render sp from db, chinh style

--- views/sanpham.php
+++ views/sanpham.php
@@ -82,19 +82,22 @@
 									foreach ($data1 as $value) {
 
 									?>
-										 <div class="col-4 products-item">
-                                            <a href="products-details.html"><img src="<?php echo $value['hinhanh_sp'];?>" alt="..."></a>
-                                            <a href="products-details.html"><h4><?php echo $value['ten_sp'];?></h4></a>
-                                            <div class="rating">
-                                                
-                                                <i class="fa fa-star" ></i>
-                                                <i class="fa fa-star" ></i>
-                                                <i class="fa fa-star" ></i>
-                                                <i class="fa fa-star-half-o" ></i>
-                                                <i class="fa fa-star-o" ></i>
+										 <!-- <div class="col-4 products-item"> -->
+										 <div class="col-4 product-shadow__hover">
+                                            <div class="products-item">
+                                                <a href="products-details.html"><img src="<?php echo $value['hinhanh_sp'];?>" alt="..."></a>
+                                                <div class="product-item__info">
+                                                    <a href="products-details.html"><h4 class="product-item__title"><?php echo $value['ten_sp'];?></h4></a>
+                                                    <div class="rating">
+                                                        <i class="fa fa-star" ></i>
+                                                        <i class="fa fa-star" ></i>
+                                                        <i class="fa fa-star" ></i>
+                                                        <i class="fa fa-star-half-o" ></i>
+                                                        <i class="fa fa-star-o" ></i>
+                                                    </div>
+                                                    <p class="product-item__price" class="product-item__price"><?php echo number_format($value['gia_sp'], 0, ',', '.');?>đ</p>
+                                                </div>
                                             </div>
-                                            <p><?php echo $value['gia_sp'];?> VND</p>
-                                            <a href="#" class="btn btn-primary">Mua hàng</a>
                                         </div>
 									<?php
 									}
@@ -108,111 +111,32 @@
             <!-------------- new row----------------->
             <h2 class="title">Sản phẩm liên quan</h2>
             <div class="row">
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-5.jpg"></a>
-                        <h4>Giày đế bệt màu xám</h4>
-                        <div class="rating">
-                            
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-half-o" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$50.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-3.jpg"></a>
-                        <h4>Giày buộc dây màu đen</h4>
-                        <div class="rating">
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-half-o" ></i>
-                        </div>
-                        <p>$21.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-7.jpg"></a>
-                        <h4>Vớ cotton nam HRX</h4>
-                        <div class="rating">
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$09.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-2.jpg"></a>
-                        <h4>Giày chạy bộ buộc dây</h4>
-                        <div class="rating">
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-o" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$35.00</p>
-                    </div>  
-                </div>
-            <!--new row for the latest product-->
-                <div class="row">
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-7.jpg"></a>
-                        <h4>Vớ cotton HRX</h4>
-                        <div class="rating">
-                            
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-half-o" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$10.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-10.jpg"></a>
-                        <h4>Giày buộc dây phẳng</h4>
-                        <div class="rating">
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-half-o" ></i>
-                        </div>
-                        <p>$48.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-11.jpg"></a>
-                        <h4>Giày Lười Nam (Xám)</h4>
-                        <div class="rating">
-                            <i class="fa fa-star-o" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$15.00</p>
-                    </div>
-                    <div class="col-4">
-                        <a href="products-details.html"><img src="images/product-12.jpg"></a>
-                        <h4>Giày trắng buộc dây</h4>
-                        <div class="rating">
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star" ></i>
-                            <i class="fa fa-star-o" ></i>
-                            <i class="fa fa-star-o" ></i>
-                        </div>
-                        <p>$21.00</p>
-                    </div>  
+                    <?php
+                        for ($i=0; $i < 8; $i++) { 
+                            ?>
+                            <div class="col-4 product-shadow__hover">
+                                <div class="products-item">
+                                    <a href="products-details.html"><img src="images/product-<?=$i+1?>.jpg" alt="..."></a>
+                                    <div class="product-item__info">
+                                        <a href="products-details.html"><h4 class="product-item__title"><?php echo $value['ten_sp'];?></h4></a>
+                                        <div class="rating">
+                                            <i class="fa fa-star" ></i>
+                                            <i class="fa fa-star" ></i>
+                                            <i class="fa fa-star" ></i>
+                                            <i class="fa fa-star-half-o" ></i>
+                                            <i class="fa fa-star-o" ></i>
+                                        </div>
+                                        <p class="product-item__price" class="product-item__price"><?php echo number_format($value['gia_sp'], 0, ',', '.');?>đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        } 
+                    ?>
                 </div>
             
             <div class="page-btn">
-                <span>1</span>
+                <span class="page-btn__active">1</span>
                 <span>2</span>
                 <span>3</span>
                 <span>4</span>
